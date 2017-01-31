03ae17ed055bdf7b650e50bd411e5eb7ccc5eccd
bbofrk <mingliwanglau@gmail.com>
2017-01-31 17:35:31 +1300

    new user and alert others

--- server/server.js
+++ server/server.js
@@ -33,13 +33,33 @@ io.on('connection', (socket) => {
 	// socket.on('createEmail', (newEmail) => {
 	// 	console.log('createEmail', newEmail);
 	// });
+
+	// socket.emit from Admin text Welcome to the chat app
+	socket.emit('newMessage', {
+		from: "Admin",
+		text: "Welcome to the chat app",
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: "Admin",
+		text: "New user joined",
+		createdAt: new Date().getTime()
+	});
+	// socket.broadcast.emit from admin text New user joined
+
 	socket.on('createMessage', (message) => {
 		console.log('create message', message);
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		// io.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
