6972a1f0ddaaa6ea79553f7a69c58967c906513a
bbofrk <mingliwanglau@gmail.com>
2017-01-31 13:51:00 +1300

    add custom event handler

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+socket.on('connect', function() {
+	console.log('connected to server');
+	// socket.emit('createEmail', {
+	// 	to: "jen@example.com",
+	// 	text: "Hey. This is Andrew"
+	// });
+
+	socket.emit('createMessage', {
+		from: "Andrew"
+		text: "Hey. This is Andrew"
+	});
+});
+
+socket.on('disconnect', function() {
+	console.log('disconnected from server');
+});
+
+// socket.on('newEmail', function(email) {
+// 	console.log('new email', email);
+// });
+
+socket.on('newMessage', function(message) {
+	console.log('got new message', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	// socket.emit('newEmail', {
+	// 	from: "mike@example.com",
+	// 	text: "What is going on?",
+	// 	createdAt: 123
+	// });
+	//
+	// socket.on('createEmail', (newEmail) => {
+	// 	console.log('createEmail', newEmail);
+	// });
+
+	socket.emit('newMessage', {
+		from: "Andrew",
+		text: "Can you meet at 6?",
+		createdAt: 123
+	});
+
+	// socket.on('createEmail', (newEmail) => {
+	// 	console.log('createEmail', newEmail);
+	// });
+	socket.on('createMessage', (message) => {
+		console.log('create message', message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('client disconnected');
 	});
